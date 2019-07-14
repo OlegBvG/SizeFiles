3369c95a6bac234edd5d8431b80bc02329994823
OlegBvG <borisov_og@mail.ru>
2019-07-14 23:45:19 +0300

    New version Stream

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,21 @@
 import java.io.*;
 import java.nio.file.*;
-import java.nio.file.attribute.*;
-import java.util.Optional;
-import java.util.Spliterator;
-import java.util.stream.Stream;
-
-import static java.nio.file.Files.isDirectory;
-import static java.nio.file.Files.size;
 
 public class Main {
 
-    static long sizeAllFile = 0;
-
     public static void main(String[] args) throws IOException {
+
         String dirname = "..\\SizeFiles";
         System.out.println("Дepeвo каталогов, начиная с каталога: " + dirname + ":\n");
         Path dirName = Paths.get(dirname);
 
-        try {
-            Files.walkFileTree(dirName, new MyFileVisitor());
-        } catch (IOException exc) {
-            System.out.println("Oшибкa ввода-вывода");
-        }
-        System.out.println("Размер всех файлов: " + humanReadableBytes(sizeAllFile));
+        Long sizeDir = Files.walk(dirName)
+                .map(Path::toFile)
+                .filter(File::isFile)
+                .mapToLong(File::length)
+                .sum();
 
-        Long sizeAllFilesStream;
-        /*
-           sizeAllFilesStream = Files.walk(dirName).reduce(0, (a, b) -> {
-            try {
-                return size(a) + size(b);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } );  // Может подскажете, что здесь требуется вернуть?
-       */
-
-        try (Stream<Path> entries = Files.walk(dirName)) {
-             sizeAllFilesStream = entries.mapToLong(Main::applyAsLong).sum();
-        }
-        System.out.println("Размер всех файлов (метод Stream): " + humanReadableBytes(sizeAllFilesStream));
+        System.out.println("Размер всех файлов: " + humanReadableBytes(sizeDir));
     }
 
      static String humanReadableBytes(long sizeFile) {
@@ -60,20 +37,4 @@ public class Main {
         return message;
     }
 
-    private static long applyAsLong(Path path) {
-        try {
-          return  isDirectory(path) ? 0 : size(path);
-        } catch (IOException e) {   System.out.println("Oшибкa ввода-вывода");
-        }
-        return 0;
-    }
 }
-
-    class MyFileVisitor extends SimpleFileVisitor<Path> {
-        public FileVisitResult visitFile(Path path, BasicFileAttributes attribs) throws IOException {
-            System.out.print(path);
-            System.out.println(" Paзмep файла: " + Main.humanReadableBytes(attribs.size()));
-            Main.sizeAllFile += attribs.size();
-            return FileVisitResult.CONTINUE;
-        }
-    }
